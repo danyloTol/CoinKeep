bf37c03e11b6de3396cb64662c6890cd1e1a4903
danyloTol <tolochkodanylo.dev@gmail.com>
2026-07-07 09:54:04 +0300

    feat: add navigation buttons to menu

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,8 +1,10 @@
-import { Icon } from '../components/Icon'   
+import { Icon } from '../components/Icon';
 import { useState } from 'react';
 
 const SideBar = () => {
     const [isMenuOpened, setIsMenuOpened] = useState(false);
+
+    const navButtonsClassName = "flex flex-row gap-3 items-center text-[26px]";
 
     return (
         <>
@@ -21,9 +23,49 @@ const SideBar = () => {
             <div className={`fixed inset-0 z-50 h-screen w-[75%] bg-bg-sidebar transition-transform duration-300 ease-in-out ${
                 isMenuOpened ? 'translate-x-0' : '-translate-x-full'
             }`}>
-                <button onClick={() => setIsMenuOpened(false)} className="p-4">
-                    <Icon name="close" className='!text-[50px] text-text-primary-light' />
-                </button>
+                <div className='flex flex-col h-full items-start px-5 py-3'>
+                    <button onClick={() => setIsMenuOpened(false)}>
+                        <Icon name="close" className='!text-[40px] text-text-primary-light' />
+                    </button>
+                    <div className='flex flex-col h-full justify-between px-1 py-5'>
+                        <div className='flex flex-col gap-2 items-start'>
+                            <button className={navButtonsClassName}>
+                                <Icon name="dashboard" className='!text-[30px]' />
+                                Dashboard
+                            </button>
+                            <button className={navButtonsClassName}>
+                                <Icon name="swap_horiz" className='!text-[30px]' />
+                                Transactions
+                            </button>
+                            <button className={navButtonsClassName}>
+                                <Icon name="wallet" className='!text-[30px]' />
+                                Wallet
+                            </button>
+                            <button className={navButtonsClassName}>
+                                <Icon name="target" className='!text-[30px]' />
+                                Goals
+                            </button>
+                            <button className={navButtonsClassName}>
+                                <Icon name="paid" className='!text-[30px]' />
+                                Budget
+                            </button>
+                            <button className={navButtonsClassName}>
+                                <Icon name="settings" className='!text-[30px]' />
+                                Settings
+                            </button>
+                        </div>
+                        <div className='flex flex-col gap-2 items-start'>
+                            <button className={navButtonsClassName}>
+                                <Icon name="help" className='!text-[30px]' />
+                                Help
+                            </button>
+                            <button className={navButtonsClassName}>
+                                <Icon name="logout" className='!text-[30px]' />
+                                Log out
+                            </button>
+                        </div>
+                    </div>
+                </div>
             </div>
 
             {/* Menu background */}
@@ -37,4 +79,4 @@ const SideBar = () => {
     )
 }
 
-export default SideBar
+export default SideBar;
