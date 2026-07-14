8d38176bdd767be008f20694e9519ed8a65e6314
danyloTol <tolochkodanylo.dev@gmail.com>
2026-07-14 20:37:38 +0300

    feat: automate ProgressBar and WidgetMSavingGoals

--- src/components/dashboard/dashboardCardsM/ProgressBar.tsx
+++ src/components/dashboard/dashboardCardsM/ProgressBar.tsx
@@ -1,15 +1,17 @@
 interface ProgressBarProps {
-    percentage: number;
+    goal: number;
+    amount: number;
     bgColor: string;
     color: string;
 }
 
-export const ProgressBar = ({percentage, bgColor, color}: ProgressBarProps) => {
+export const ProgressBar = ({goal, amount, bgColor, color}: ProgressBarProps) => {
     return (
         <div className={`w-full ${bgColor} rounded-full h-4.5 overflow-hidden`}>
-            <div className={`flex flex-col items-center rounded-full h-full ${color} transition-all duration-500 ease-out`}
-                style={{width: `${percentage}%`}}>
-                <span className="text-[13px]">{percentage}%</span>
+            <div className={`flex flex-col min-w-7 items-center rounded-full h-full ${color} transition-all duration-500 ease-out`}
+                style={{width: `${(amount / (goal / 100) > 100) ? 100 : amount / (goal / 100) }%`}}>
+                    
+                <span className="text-[13px] px-10">{Math.round((amount / (goal / 100) > 100) ? 100 :((amount / (goal / 100))*10) / 10)}%</span>
             </div>
         </div>
     )
--- src/components/dashboard/dashboardCardsM/WidgetMSavingGoals.tsx
+++ src/components/dashboard/dashboardCardsM/WidgetMSavingGoals.tsx
@@ -1,33 +1,30 @@
 import { ProgressBar } from './ProgressBar'
 
-const bgProgressBar = 'bg-brand-light';
-const progressBarClr = 'bg-brand';
+const goalsList = [
+    {name: 'MacBook Pro', goal: 150000, amount: 91500},
+    {name: 'New car', goal: 150000, amount: 95000},
+    {name: 'New house', goal: 150000, amount: 300000},
+]
 
 export const WidgetMSavingGoals = () => {
+    const formatted = (num: number) => {
+        return num.toLocaleString('en-US', {
+            minimumFractionDigits: 2,
+            maximumFractionDigits: 2
+    })};
+
     return (
-        <div className="flex flex-col gap 4.75 px-6.25 w-full outline-1 outline-text-secondary-light rounded-[30px]">
+        <div className="flex flex-col gap 4.75 px-6.25 py-3 w-full outline-1 outline-text-secondary-light rounded-[30px]">
             <h1 className='text-[24px] text-text-primary-light'>Saving goals</h1>
-            <div>
-                <div className='w-full flex flex-row justify-between'>
-                    <p className='text-[13px] text-text-primary-light'>MacBook Pro</p>
-                    <p className='text-[13px] text-brand'>$150,000</p>
+            {goalsList.map((item) => (
+                <div>
+                    <div className='w-full flex flex-row justify-between'>
+                        <p className='text-[13px] text-text-primary-light'>{item.name}</p>
+                        <p className='text-[13px] text-brand'>${formatted(item.goal)}</p>
+                    </div>
+                    <ProgressBar bgColor='bg-brand-light' color='bg-brand' goal={item.goal} amount={item.amount} />
                 </div>
-                <ProgressBar bgColor={bgProgressBar} color={progressBarClr} percentage={25} />
-            </div>
-            <div>
-                <div className='w-full flex flex-row justify-between'>
-                    <p className='text-[13px] text-text-primary-light'>New car</p>
-                    <p className='text-[13px] text-brand'>$150,000</p>
-                </div>
-                <ProgressBar bgColor={bgProgressBar} color={progressBarClr} percentage={42} />
-            </div>
-            <div>
-                <div className='w-full flex flex-row justify-between'>
-                    <p className='text-[13px] text-text-primary-light'>New house</p>
-                    <p className='text-[13px] text-brand'>$150,000</p>
-                </div>
-                <ProgressBar bgColor={bgProgressBar} color={progressBarClr} percentage={6} />
-            </div>
+            ))}
         </div>
     );
 };
