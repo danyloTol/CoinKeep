fd54e4e6f51fa0863f287d0fb7db9a801b757624
danyloTol <tolochkodanylo.dev@gmail.com>
2026-07-14 12:44:27 +0300

    feat: add TotalBalance widget to small card for dashboard

--- src/components/dashboard/dashboardCardsS/DashboardCardS.tsx
+++ src/components/dashboard/dashboardCardsS/DashboardCardS.tsx
@@ -1,18 +1,34 @@
-const DashboardCardS = () => {
-    return (
-        <div className="flex flex-col gap-4 py-2 px-6.25 outline-1 outline-text-secondary-light rounded-[30px]">
-            <div>
-                <h3 className="text-[14px]">Total balance</h3>
-            </div>
-            <div>
-                <h2 className="text-text-primary-light text-[24px]">$17,854<span className="text-text-secondary-light">.38</span></h2>
-            </div>
-            <div className="flex flex-row items-center justify-between">
-                <p className="w-max px-1.25 py-0.75 rounded-full bg-bg-success text-text-success text-[12px]">⬆ 12.1%</p>
-                <p className="text-[10px] text-text-secondary-light">vs last month</p>
-            </div>
-        </div>
-    )
+import { lazy, Suspense } from "react";
+
+const TotalBalance = lazy(() => import('./WidgetSTotalBalance'));
+
+interface WidgetProps {
+    ThisMonthValue: number;
+    LastMonthValue: number;
 }
+
+const WIDGET_MAP: Record<string, React.LazyExoticComponent<React.FC<WidgetProps>>> = {
+    totBalance: TotalBalance,
+};
+
+interface DashboardCardSProps extends WidgetProps {
+    type: 'totBalance';
+}
+
+const DashboardCardS = ({ type, ...props }: DashboardCardSProps) => {
+    const WidgetComponent = WIDGET_MAP[type];
+
+    return (
+        <div>
+            {WidgetComponent ? (
+                <Suspense fallback={<div>Завантаження...</div>}>
+                    <WidgetComponent {...props} />
+                </Suspense>
+            ) : (
+                <div>Widget not found</div>
+            )}
+        </div>
+    );
+};
 
 export default DashboardCardS
---
+++ src/components/dashboard/dashboardCardsS/WidgetSTotalBalance.tsx
@@ -0,0 +1,34 @@
+interface WidgetProps {
+    ThisMonthValue: number;
+    LastMonthValue: number;
+}
+
+const WidgetSTotalBalance = ({ThisMonthValue, LastMonthValue}: WidgetProps) => {
+    const formatted = ThisMonthValue.toLocaleString('en-US', {
+        minimumFractionDigits: 2,
+        maximumFractionDigits: 2
+    });
+
+    const [integer, decimal] = formatted.split('.');
+
+    const changePercent = Math.round((ThisMonthValue - LastMonthValue) / LastMonthValue * 100) / 10
+
+    return (
+        <div className="flex flex-col gap-4 py-2 px-6.25 outline-1 outline-text-secondary-light rounded-[30px] overflow-hidden">
+            <div>
+                <h3 className="text-[14px]">Total balance</h3>
+            </div>
+            <div>
+                <h2 className="text-text-primary-light text-[24px]">${integer}.<span className="text-text-secondary-light">{decimal}</span></h2>
+            </div>
+            <div className="flex flex-row items-center justify-between">
+                {(changePercent >= 0) ? 
+                    <p className="w-max px-1.25 py-0.75 rounded-full bg-bg-success text-text-success text-[11px] whitespace-nowrap">↑ {changePercent}%</p> :
+                    <p className="w-max px-1.25 py-0.75 rounded-full bg-bg-danger text-text-danger text-[11px] whitespace-nowrap">↓ {changePercent}%</p>}
+                <p className="text-[10px] text-text-secondary-light whitespace-nowrap">vs last month</p>
+            </div>
+        </div>
+    )
+}
+
+export default WidgetSTotalBalance
