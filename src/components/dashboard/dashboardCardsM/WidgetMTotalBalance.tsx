b4c923b28c78c17c5de77cd9817d3eebfeee3cd3
danyloTol <tolochkodanylo.dev@gmail.com>
2026-07-15 15:47:46 +0300

    style: add style to change percent icon in Total balance widget

--- src/components/dashboard/dashboardCardsM/WidgetMTotalBalance.tsx
+++ src/components/dashboard/dashboardCardsM/WidgetMTotalBalance.tsx
@@ -16,13 +16,20 @@ const WidgetMTotalBalance = ({ thisMonthValue, lastMonthValue }: WidgetProps) =>
     const changePercent = (thisMonthValue !== undefined && lastMonthValue !== undefined) && Math.round((thisMonthValue - lastMonthValue) / lastMonthValue * 100);
 
     return (
-        <div className="flex flex-col gap-4 py-2 px-6.25 outline-1 outline-text-secondary-light rounded-[30px] overflow-hidden">
+        <div className="flex flex-row gap-4 py-2 px-6.25 outline-1 outline-text-secondary-light rounded-[30px] overflow-hidden">
             <div className="flex flex-col gap-2">
                 <h1 className='text-[24px] text-text-primary-light'>Total balance</h1>
                 <h1 className="text-[28px]"><span>${integer}</span><span className="text-text-secondary-light">.{decimal}</span></h1>
             </div>
             <div>
-                {(typeof changePercent === 'number' && Number.isFinite(changePercent)) && (changePercent > 0) ? <Icon name='arrow_upward_alt' /> : <Icon name='arrow_upward_alt' className='rotate-180' />}
+                <div className={`flex flex-row items-center gap-2 rounded-full px-2 py-0.5 ${(typeof changePercent === 'number' && Number.isFinite(changePercent)) ? "bg-bg-success text-text-success" : "bg-bg-danger text-text-danger"}`}>
+                    {(typeof changePercent === 'number' && Number.isFinite(changePercent)) && 
+                    (changePercent > 0) ? 
+                        <Icon name='arrow_upward_alt' className='!text-[30px] rounded-full' /> : 
+                        <Icon name='arrow_upward_alt' className='rotate-180' />}
+                        <p>{(typeof changePercent === 'number' && Number.isFinite(changePercent)) && "+"}{changePercent}%</p>
+                </div>
+                
             </div>
         </div>
     )
