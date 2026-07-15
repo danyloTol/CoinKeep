499d884d10764f43e7d65add555c87471daea93f
danyloTol <tolochkodanylo.dev@gmail.com>
2026-07-15 13:33:56 +0300

    fix: fix decimal part in goal label in Saving goals widget

--- src/components/dashboard/dashboardCardsM/WidgetMSavingGoals.tsx
+++ src/components/dashboard/dashboardCardsM/WidgetMSavingGoals.tsx
@@ -3,7 +3,7 @@ import { ProgressBar } from './ProgressBar'
 const goalsList = [
     {name: 'MacBook Pro', goal: 150000, amount: 91500},
     {name: 'New car', goal: 150000, amount: 95000},
-    {name: 'New house', goal: 150000, amount: 300000},
+    {name: 'New house', goal: 90000, amount: 300000},
 ]
 
 export const WidgetMSavingGoals = () => {
@@ -11,20 +11,31 @@ export const WidgetMSavingGoals = () => {
         return num.toLocaleString('en-US', {
             minimumFractionDigits: 2,
             maximumFractionDigits: 2
-    })};
+        });
+    };
 
     return (
-        <div className="flex flex-col gap 4.75 px-6.25 py-3 w-full outline-1 outline-text-secondary-light rounded-[30px]">
+        <div className="flex flex-col gap-4.75 px-6.25 py-3 w-full outline-1 outline-text-secondary-light rounded-[30px]">
             <h1 className='text-[24px] text-text-primary-light'>Saving goals</h1>
-            {goalsList.map((item) => (
-                <div>
-                    <div className='w-full flex flex-row justify-between'>
-                        <p className='text-[13px] text-text-primary-light'>{item.name}</p>
-                        <p className='text-[13px] text-brand'>${formatted(item.goal)}</p>
+            {goalsList.map((item) => {
+                const formattedValue = formatted(item.goal);
+                const [integer, decimal] = formattedValue.split('.');
+
+                return (
+                    <div key={item.name}>
+                        <div className='w-full flex flex-row justify-between'>
+                            <p className='text-[13px] text-text-primary-light'>{item.name}</p>
+                            <p className='text-[13px] text-brand'>${integer}</p>
+                        </div>
+                        <ProgressBar 
+                            bgColor='bg-brand-light' 
+                            color='bg-brand' 
+                            goal={item.goal} 
+                            amount={item.amount} 
+                        />
                     </div>
-                    <ProgressBar bgColor='bg-brand-light' color='bg-brand' goal={item.goal} amount={item.amount} />
-                </div>
-            ))}
+                );
+            })}
         </div>
     );
 };
